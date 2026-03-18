0fcd6ab8720c0921f356630b0e54a37c6bfc493e
farrel-11 <23502410011@student.prasetiyamulya.ac.id>
2026-03-19 03:15:46 +0700

    feat: Menyelesaikan seluruh UI alur Login dan Autentikasi
    - Membuat halaman Sign In (signin.dart)
    - Menambahkan halaman RIPLAY (riplay.dart)
    - Membuat form input email dengan validasi Regex (masuk_screen.dart)
    - Membuat halaman OTP 6 digit dengan auto-focus otomatis (verifikasi_code_screen.dart)
    - Membuat panduan Verifikasi Wajah dan wadah kamera dummy (verifikasi_wajah_screen.dart, face_recog_screen.dart)
    - Membuat halaman Login untuk user lama dengan fitur hide/show password (login_page.dart)
    - Membuat halaman Lupa Password (lupa_password_screen.dart)
    - Menstandarkan warna abu-abu transparan untuk kotak input di colors.dart

---
+++ Frontend/ikebank/lib/screens/auth/login/login_page.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import '../../../core/colors.dart';
+import 'lupa_password_screen.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle alumniSansBold = TextStyle(fontWeight: FontWeight.w700);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.43,
+              width: double.infinity,
+              child: Image.asset(
+                'assets/images/IKEBank.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            Container(
+              width: double.infinity,
+              transform: Matrix4.translationValues(0.0, -30.0, 0.0),
+              padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center, 
+                children: [
+                  
+                  const SizedBox(height: 70), 
+
+                  Text(
+                    "Selamat Datang Kembali",
+                    style: alumniSansBold.copyWith(
+                      fontSize: 32,
+                      color: AppColors.primaryOrange,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Ikuti langkah dibawah ini untuk masuk kembali ke akunmu",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: AppColors.primaryOrange.withValues(alpha: 0.9), 
+                      height: 1.4,
+                    ),
+                  ),
+                  
+                  const SizedBox(height: 110), 
+
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFD9D9D9).withValues(alpha: 0.5), 
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: TextField(
+                      obscureText: !_isPasswordVisible, 
+                      style: const TextStyle(fontSize: 18),
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        hintStyle: const TextStyle(color: Colors.black, fontSize: 16),
+                        border: InputBorder.none,
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.black54,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  
+                  const SizedBox(height: 8),
+
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const LupaPasswordScreen()));
+                      },
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                        minimumSize: const Size(0, 0),
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      ),
+                      child: Text(
+                        "Lupa Password?",
+                        style: alumniSansBold.copyWith(
+                          fontSize: 15,
+                          color: AppColors.primaryOrange,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 16), 
+
+                  // TOMBOL MASUK & FINGERPRINT
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFCDCDCD), 
+                            elevation: 0,
+                            padding: const EdgeInsets.symmetric(vertical: 18),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12), 
+                            ),
+                          ),
+                          onPressed: () {
+                          },
+                          child: Text(
+                            "Masuk",
+                            style: alumniSansBold.copyWith(
+                              fontSize: 20,
+                              color: Colors.white, 
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Fingerprint 
+                      const SizedBox(width: 16),
+                      Container(
+                        height: 60, 
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: AppColors.primaryOrange,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(Icons.fingerprint, color: Colors.white, size: 32),
+                          onPressed: () {
+                            // TODO: Trigger Biometric Login
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
