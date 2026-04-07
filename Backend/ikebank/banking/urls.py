0b3bde8161707571f40c5d46e7e0d6cf534f8095
Benedicto Fernandez Ojan <bradfo.fernandez@gmail.com>
2026-04-07 22:39:36 +0700

    menghapus folder backend dari feature-kartu-ui

--- Backend/ikebank/banking/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import CashFlowCalculateView, RegisterBankAccountView
-
-
-urlpatterns = [
-    path ('register/', RegisterBankAccountView.as_view(), name='register-bank-account'),
-	path('cashflow/calculate/', CashFlowCalculateView.as_view(), name='cashflow-calculate'),
-]
