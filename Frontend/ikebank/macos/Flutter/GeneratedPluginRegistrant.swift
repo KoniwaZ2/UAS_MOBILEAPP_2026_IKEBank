116ff3b243fa6dbd1c5f14805203cf7b4cb92085
farrel-11 <23502410011@student.prasetiyamulya.ac.id>
2026-04-15 12:50:14 +0700

    push

--- Frontend/ikebank/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/ikebank/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import flutter_local_notifications
 import flutter_secure_storage_macos
 import local_auth_darwin
 import mobile_scanner
@@ -15,7 +14,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
